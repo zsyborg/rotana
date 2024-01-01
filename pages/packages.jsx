7c392c52f85193a4898ddabd69ccb170ead5bcea
Zasha <zahid.shaikh@live.com>
2024-01-01 21:37:24 +0530

    Added all packages

--- pages/packages.jsx
+++ pages/packages.jsx
@@ -20,8 +20,18 @@ const Food = () => {
                 <h2 id="umrah">Umrah Packages</h2>
                 <img src="assets/images/packages/1.jpg" className="img-fluid" width='100%'/><br/>
                 <img src="assets/images/packages/2.jpg" className="img-fluid" width='100%'/><br/>
-                <img src="assets/images/packages/3.jpg" className="img-fluid" width='100%'/>
-                <img src="assets/images/packages/B2B.jpg" className="img-fluid" width='100%'/>
+                <img src="assets/images/packages/3.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/B2B.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/med/1.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/med/2.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/med/3.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/mak/1.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/mak/2.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/mak/3.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/mak/4.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/mak/5.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/B2B.jpg" className="img-fluid" width='100%'/><br/>
+                <img src="assets/images/packages/B2B.jpg" className="img-fluid" width='100%'/><br/>
                 <img src="assets/images/hotels/Rotana 2 page_page-0002.jpg" className="img-fluid"/>
                 <img src="assets/images/hotels/Rotana 2 page_page-0001.jpg" className="img-fluid"/>
               </div>
